fbc522a47c6c9f280b2f7f831a518b5dddc550f9
derpatrick <patrick.piatkowski@gmail.com>
2018-03-06 15:24:46 +0100

    add angular2-draggable, remove dragula little example for free button dragging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ComponentsModule } from '../components/components.module';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ComponentsModule} from '../components/components.module';
+import {AngularDraggableModule} from "angular2-draggable";
 
 @NgModule({
   declarations: [MyApp, HomePage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp), ComponentsModule],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), ComponentsModule, AngularDraggableModule],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage],
   providers: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  bag1: string[];
+  bag2: string[];
 
+  constructor(public navCtrl: NavController) {
+    this.bag1 = ['PotatOS', 'GLaDOS', 'Chell'];
+    this.bag2 = ['Atlas']
   }
 
 }
