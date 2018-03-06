a36061003a9ecbc032e49dc2f8ad58eeb188e2e1
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-03-06 14:36:20 +0100

    rename MyApp root component (app.component) to ScaleITDomainApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
+import { ScaleITDomainApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
-  declarations: [MyApp, HomePage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp), ComponentsModule],
+  declarations: [ScaleITDomainApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(ScaleITDomainApp), ComponentsModule],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, HomePage],
+  entryComponents: [ScaleITDomainApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
