4dc9e80a538fa7660e0c2cbe3ee21460eed87725
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-04-05 13:34:03 +0200

    fixed 10; data provider example

--- src/pages/administration/administration.ts
+++ src/pages/administration/administration.ts
@@ -18,9 +18,22 @@ import { HttpDataProvider } from '../../providers/http-data/http-data';
   templateUrl: "administration.html"
 })
 export class AdministrationPage {
+  data: any[];
   constructor(public navCtrl: NavController, public navParams: NavParams, public dataProvider: HttpDataProvider) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad AdministrationPage");
+
+console.log("Fetching data...");
+    let dataObserver = this.dataProvider.getData();
+
+    dataObserver
+      .subscribe(dataFromProvider => {
+        console.log("Data received:" + dataFromProvider);
+        this.data = dataFromProvider
+          .filter(x => x["type"] === 'someData') // get only certain data points;
+          .map(x => x["name"]); // get only the name string;
+        console.log("Data filtered:" + this.data);
+      });
   }
 }
