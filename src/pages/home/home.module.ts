dbad0893ba5756c4dceabccf1373f2caa9cfd4cf
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-04-07 14:55:10 +0200

    removed unused import

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { IonicPageModule, IonicModule } from "ionic-angular";
+import { IonicPageModule } from "ionic-angular";
 import { HomePage } from "./home";
 import { ComponentsModule } from "../../components/components.module";
 import { PipesModule } from "../../pipes/pipes.module";
