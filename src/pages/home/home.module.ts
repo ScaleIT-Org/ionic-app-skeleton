6edce38846e4d26ccf48f98e19ea79f5f5b9d505
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-04-07 15:25:25 +0200

    dynamic translation example with ngx-translate

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -3,10 +3,28 @@ import { IonicPageModule } from "ionic-angular";
 import { HomePage } from "./home";
 import { ComponentsModule } from "../../components/components.module";
 import { PipesModule } from "../../pipes/pipes.module";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { HttpClient } from "@angular/common/http";
 
 @NgModule({
   declarations: [HomePage],
-  imports: [IonicPageModule.forChild(HomePage), ComponentsModule, PipesModule],
+  imports: [
+    IonicPageModule.forChild(HomePage),
+    ComponentsModule,
+    PipesModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
   bootstrap: [HomePage]
 })
 export class HomePageModule {}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,19 +1,28 @@
-import { IonicPage } from "ionic-angular";
-import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
+import { IonicPage        } from "ionic-angular"      ;
+import { Component        } from "@angular/core"      ;
+import { NavController    } from "ionic-angular"      ;
+import { TranslateService } from "@ngx-translate/core";
 
 @IonicPage({
-  name: "user",
-  segment: "user"
+  name    : "user",
+  segment : "user"
 })
 @Component({
-  selector: "page-home",
-  templateUrl: "home.html"
+  selector    : "page-home",
+  templateUrl : "home.html"
 })
 export class HomePage {
   today: number;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public  navCtrl   : NavController    ,
+    private translate : TranslateService
+  ) {
     this.today = Date.now();
+  }
+
+  // translation usage example from https://github.com/ngx-translate/ionic-example
+  public changeLanguage(language) {
+    this.translate.use(language);
   }
 }
