88b8d240034b39a1f764809178ec668cef598169
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-04-07 15:24:49 +0200

    back button text works, comment removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { ScaleITDomainApp } from './app.component';
-import { HomePageModule } from '../pages/home/home.module';
-import { HttpDataProvider } from '../providers/http-data/http-data';
-import { HttpClientModule } from '@angular/common/http';
+import { ScaleITDomainApp } from "./app.component";
+import { HomePageModule } from "../pages/home/home.module";
+import { HttpDataProvider } from "../providers/http-data/http-data";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   // somehow ionicons get busted when doing lazy loaded pages (declare HomePage here and they work)
@@ -15,7 +15,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     IonicModule.forRoot(ScaleITDomainApp, {
-      //not working
       backButtonText: ""
     }),
     // because the root page component is lazy loaded, it must be imported in the root module
