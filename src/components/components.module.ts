2be0f37e2e56d983259daecb98344100f0f1357f
Kristina Katovich <kristinakatovich@gmail.com>
2018-05-24 00:39:09 +0200

    added menu as a component to app-header

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 import { IonicModule } from "ionic-angular";
 import { AppFooterComponent } from './app-footer/app-footer';
 import { AppHeaderComponent } from './app-header/app-header';
+import { AppMenuComponent } from './app-menu/app-menu';
 @NgModule({
   declarations: [
     AppFooterComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppMenuComponent
   ],
   imports: [
     IonicModule
   ],
   exports: [
     AppFooterComponent,
-    AppHeaderComponent
+    AppHeaderComponent,
+    AppMenuComponent
   ]
 })
 export class ComponentsModule {}
